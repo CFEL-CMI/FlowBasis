0b5fe6e0914b38f2fad72446ceb41156a8dd6186
Yahya Saleh <37277120+Saleh0694@users.noreply.github.com>
2022-10-10 09:09:03 +0200

    Update __init__.py

--- flowbasis/__init__.py
+++ flowbasis/__init__.py
@@ -1,1 +1,18 @@
-#initializer
+#!/usr/bin/env python3
+# -*- coding: utf-8; fill-column: 100; truncate-lines: t -*-
+#
+# This file is part of FlowBasis python package
+#
+# Copyright (C) 2018,2020 CFEL Controlled Molecule Imaging group
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# If you use this program for scientific work, you should correctly reference it; see the LICENSE.md file for details.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
