54e501f4faa73c36be9fa0439253ae1e25bd71c3
Yahya Saleh <37277120+Saleh0694@users.noreply.github.com>
2022-10-10 09:04:21 +0200

    added description

--- flowbasis/quadratures.py
+++ flowbasis/quadratures.py
@@ -1,5 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8; fill-column: 100; truncate-lines: t -*-
+#
+# This file is part of Active-Learning-of-PES python package
+#
+# Copyright (C) 2018,2020 CFEL Controlled Molecule Imaging group
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# If you use this program for scientific work, you should correctly reference it; see the LICENSE.md file for details.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
+
+"""
+An implementation of Quadrature rules."""
+
 import numpy as np
-#import Tasmanian
 from numpy.polynomial.hermite import hermgauss
 import jax 
 from numpy.polynomial.legendre import leggauss 
